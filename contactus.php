67671d97e1bc753ffc743ed85abc09e7ad513b18
zeeshan <zeeshanrauf002@hotmail.com>
2013-11-15 03:05:25 +0500

    ASC

---
+++ contactus.php
@@ -0,0 +1,343 @@
+<!DOCTYPE html>
+<!--[if IE 6]>
+<html id="ie6">
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7">
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8">
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<html>
+<!--<![endif]-->
+<head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="viewport" content="width=device-width">
+    
+    <title>Template | American Shakespeare Center</title>
+    
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+    
+    <!--[if lte IE 8]>
+    <link type="text/css" rel="stylesheet" href="css/ie.css">
+    <script type="text/javascript" src="js/modernizr.min.js"></script>
+    <![endif]-->
+    
+    <!--[if lte IE 6]>
+    <script type="text/javascript" src="js/belated.js"></script>
+    <![endif]-->
+    
+</head>
+
+<body id="template-page">
+
+ <div class="wrapper">
+
+<div id="container-fluid" class="contactus">
+
+
+<!--main start -->
+<div id="main-wrap">
+	<div class="content-width">
+        <div id="main">
+        	<div class="asc-template clearfix">
+                <aside id="sidebar">
+                    <div class="image">
+                        <img src="images/page-image.jpg" alt="" >
+                    </div>
+                    <div class="bottom-section clearfix">
+                    	<div class="news-feed">
+                        	<h2>NEWS FEED</h2>
+                        	<ul>
+                            	<li class="clearfix">
+                                	 <div class="dates">
+                                     	<span>6.01</span>
+                                     </div>
+                                     <div class="feeds">
+                                     	<a href="#">Lorem ipsum dolor sit</a><br>
+                                        <span>Elementum nibh sit amet nisl consectetur at varius <a href="#">[ more ]</a></span>
+                                     </div>
+                                </li>
+                                <li class="clearfix">
+                                	 <div class="dates">
+                                     	<span>5.31</span>
+                                     </div>
+                                     <div class="feeds">
+                                     	<a href="#">Lorem ipsum dolor sit</a><br>
+                                        <span>Elementum nibh sit amet nisl consectetur at varius<br> lectus vulputate. Donec varius justo <a href="#">[ more ]</a></span>
+                                     </div>
+                                </li>
+                                <li class="clearfix">
+                                	 <div class="dates">
+                                     	<span>5.15</span>
+                                     </div>
+                                     <div class="feeds">
+                                     	<a href="#">Lorem ipsum dolor sit</a><br>
+                                        <span>Elementum nibh sit amet nisl consectetur at varius <a href="#">[ more ]</a></span>
+                                     </div>
+                                </li>
+                                <li class="clearfix">
+                                	 <div class="dates">
+                                     	<span>4.20</span>
+                                     </div>
+                                     <div class="feeds">
+                                     	<a href="#">Lorem ipsum dolor sit</a><br>
+                                        <span>Elementum nibh sit amet nisl consectetur at varius <a href="#">[ more ]</a></span>
+                                     </div>
+                                </li>
+                            </ul>
+                            <div class="onstage-box">
+                            	<img src="images/onstage-img.jpg" alt="" >
+                                <p><span>Lorem ipsum dolor sit</span>|<span class="date-month">MAY 30, 2013</span><br>Elementum nibh sit amet nisl consectetur at varius <a href="#">[ more ]</a></p>
+                            </div>
+                            <img src="images/2013.jpg" alt="" class="year" >
+                        </div>
+                    </div>
+                </aside><!--sidebar -->
+                <section id="contents">
+                	<div class="top-section clearfix">
+                    	<div class="page-detail clearfix">
+                            <h2>Contact Us</h2>
+                            <div class="ourtheatre">
+                            <strong>OUR THEATRE:</strong>
+                            <p>The Blackfriars Playhouse <br>10 S. Market Street <br>Staunton, VA 24401 <br>[ <a href="#">GOOGLE MAP</a> ]</p>
+                            <p>For tickets and information <br>on performances: <br>Local: (540) 851-1733 <br>Toll Free: (877) MUCH-ADO <br>(877-682-4236)</p>
+                            <p>*Required</p>
+                            </div>
+                            <div class="ourcompany">
+                            <strong>OUR COMPANY OFFICES</strong>
+                            <p>The American Shakespeare Center <br>20 S. New Street,  Fourth Floor <br>Staunton, VA 24401 <br>[ <a href="#">GOOGLE MAP</a> ]</p>
+                            <p>Phone: (540) 885-5588 <br>Fax: (540) 885-4886 </p>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="page-detail-2">
+                    	<div class="contact-form">
+                        	<p>
+                            	<label>First Name</label>
+                            	<input type="text" value="" class="input" ><span>*</span>
+                            </p>
+                            <p>
+                            	<label>Last Name</label>
+                            	<input type="text" value="" class="input" ><span>*</span>
+                            </p>
+                            <p>
+                            	<label>E-mail</label>
+                            	<input type="text" value="" class="input" ><span>*</span>
+                            </p>
+                            <p>
+                            	<label>Organization</label>
+                            	<input type="text" value="" class="input" >
+                            </p>
+                            <p>
+                            	<label>Address</label>
+                            	<input type="text" value="" class="input" >
+                            </p>
+                            <p>
+                            	<label>City</label>
+                            	<input type="text" value="" class="input" >
+                            </p>
+                            <p>
+                            	<label>State</label>
+                            	<input type="text" value="" class="input" >
+                            </p>
+                            <p>
+                            	<label>Zip</label>
+                            	<input type="text" value="" class="input" >
+                            </p>
+                            <p>
+                            	<label>Phone</label>
+                            	<input type="text" value="" class="input" >
+                            </p>
+                            <p>
+                            	<strong>Comments or Questions?</strong>
+                            </p>
+                            <p>
+                            	<textarea rows="2" cols="20"></textarea>
+                            </p>
+                            <p>
+                            	<strong>Validation</strong>
+                                <img src="images/validation-img.jpg" alt=""  class="validation">
+                                <input type="text" value="" class="validation-input" >
+                                <input type="submit" value="SUBMIT" class="submit-btn">
+                            </p>
+                        </div>	
+                    </div>
+                </section><!--contents -->
+            </div>
+        </div>
+        <div class="social-links-inner-nav clearfix">
+        	<div class="inner-nav">
+            	<ul class="clearfix">
+                	<li><a href="contact.php">CONTACT US</a></li>
+                    <li><a href="mission.php">MISSION</a></li>
+                    <li><a href="vision.php">VISION</a></li>
+                    <li class="last"><a href="on-tour.php">ON TOUR</a></li>
+                </ul>
+            </div>
+            <div class="social-media">
+                <!-- AddThis Button BEGIN -->
+                <div class="addthis_toolbox addthis_default_style ">
+                <a class="addthis_button_preferred_1"></a>
+                <a class="addthis_button_preferred_2"></a>
+                <a class="addthis_button_preferred_3"></a>
+                <a class="addthis_button_preferred_4"></a>
+                <a class="addthis_button_compact"></a>
+                <a class="addthis_counter addthis_bubble_style"></a>
+                </div>
+                <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=xa-51fe01075387ed3c"></script>
+                <!-- AddThis Button END -->
+            </div>
+        </div>
+    </div>
+</div>
+<!--main end -->
+
+<!--header start -->
+<div id="header-wrap">
+	<div class="content-width">    
+        <header id="header" class="clearfix">
+            <div id="logo">
+            	<a href="index.php"><img src="images/logo.png" alt="" ></a><a href="#"></a>
+            </div>
+            <div id="nav-ticket" class="clearfix">
+            	<!--navigation start -->
+                <ul id="nav" class="clearfix">
+                    <li>
+                        <a href="#">On Stage</a>
+                        <ul>
+                            <li><a href="now-playing.php">Now Playing</a></li>
+                            <li><a href="on-tour.php">ASC On Tour</a></li>
+                            <li><a href="special-events.php">Special Events</a></li>
+                            <li><a href="ticket-information.php">Ticket Information</a></li>
+                            <li class="last"><a href="calendar.php">Full Calendar</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">Education</a>
+                        <ul>
+                            <li><a href="camps">Camps</a></li>
+                            <li><a href="programs-for-educators.php">Educator Programs</a></li>
+                            <li><a href="letures-workshops.php">Lectures/Workshops</a></li>
+                            <li><a href="leadership.php">Leadership</a></li>
+                            <li class="last"><a href="archives.php">Archives</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">About/Visit</a>
+                        <ul>
+                            <li><a href="plan-your-visit.php">Plan Your Visit</a></li>
+                            <li><a href="what-we-do.php">What We Do</a></li>
+                            <li><a href="history.php">ASC History</a></li>
+                            <li><a href="actors-and-staff.php">Actors & Staff</a></li>
+                            <li class="last"><a href="goft-shop.php">Gift Shop</a></li>
+                        </ul>
+                    </li>
+                    <li class="last">
+                        <a href="#">Get Involved</a>
+                        <ul>
+                            <li><a href="invest-in-imagination.php">Invest in Imagination</a></li>
+                            <li><a href="giving-and-sponsorship.php">Giving & Sponsorship</a></li>
+                            <li><a href="volunteering.php">Volunteering</a></li>
+                            <li><a href="employment.php">Employment</a></li>
+                            <li class="last"><a href="board-membership.php">Board Membership</a></li>
+                        </ul>
+                    </li>
+                </ul>
+				<!--navigation end -->
+                <div class="ticket">
+                	<div class="get-tickets clearfix">
+                    	<a href="tickets.php" id="get-tickets">GET TICKETS</a>
+                        <a href="calendar.php" class="calendar">CALENDAR</a>
+                        <a href="donate.php" class="donate">DONATE</a>
+                    </div>
+                    <div class="search-bar">
+                    	<label>Search</label>
+            			<input type="text" value="" class="search-field" />
+            			<input type="button" value="" class="search-btn" />
+                    </div>
+                </div>
+            </div>
+        </header>
+    </div>
+</div>
+<!--header end -->
+
+<div class="push"></div>
+</div>
+
+</div><!--container-fluid -->
+
+<!--footer start -->
+<div id="footer-wrap">
+	<div class="content-width">
+        <footer id="footer" class="clearfix">
+        	<div id="sponsor-section">
+            	<p>We are proud to acknowledge that the Amercian Shakespeare Center receives financial support from the Virginia Commission for the Arts and the National Endowment for the Arts.</p>
+                <div class="sponsor clearfix">
+                	<ul>
+                    	<li class="first"><img src="images/sponsor-1.jpg" alt=""></li>
+                        <li><img src="images/sponsor-2.jpg" alt=""></li>
+                        <li><img src="images/sponsor-3.jpg" alt=""></li>
+                    </ul>
+                </div>
+                <p>©2013 Amercian Shakespeare Center <br>BOX OFFICE: 540.851.1733 | 1.877.MUCH.ADO</p>
+            </div>
+            <div id="footer-detail" class="clearfix">
+            	<div class="footer-sections">
+                	<h4>2013 SUMMER SEASON</h4>
+                    <ul>
+                    	<li><a href="#">Romeo and Juliet</a></li>
+                        <li><a href="#">Return to the Forbidden Planet</a></li>
+                        <li><a href="#">All's Well that Ends Well</a></li>
+                    </ul>
+                </div>
+                <div class="footer-sections">
+                	<h4>2013 FALL SEASON</h4>
+                    <ul>
+                    	<li><a href="#">Troilus and Cressida</a></li>
+                        <li><a href="#">She Stoops to Conquer</a></li>
+                        <li><a href="#">Romeo and Juliet</a></li>
+                        <li><a href="#">Return to the Forbidden<br>Planet</a></li>
+                        <li><a href="#">All's Well that Ends Well</a></li>
+                    </ul>
+                </div>
+                <div class="footer-sections">
+                	<h4>2013 HOLIDAY SEASON</h4>
+                    <ul>
+                    	<li><a href="#">The Twelve Dates of Christmas</a></li>
+                        <li><a href="#">The Stantaland Diaries</a></li>
+                        <li><a href="#">A Christmas Carol</a></li>
+                        <li><a href="#">Othello</a></li>
+                        <li><a href="#">The Merry Wives of Windsor</a></li>
+                    </ul>
+                </div>
+                <div class="footer-sections">
+                	<h4>2014 ACTORS' RENAISSANCE SEASON</h4>
+                    <ul>
+                    	<li><a href="#">As you like it</a></li>
+                        <li><a href="#">The Servent of two Masters</a></li>
+                        <li><a href="#">Timon of Athens</a></li>
+                        <li><a href="#">Epicene, or The Silent Woman</a></li>
+                        <li><a href="#">The Maid's Tragedy</a></li>
+                    </ul>
+                </div>
+                <div class="footer-sections last">
+                	<h4>2014 SPRING SEASON</h4>
+                    <ul>
+                    	<li><a href="#">Othello</a></li>
+                        <li><a href="#">The Merry Wives of Windsor</a></li>
+                        <li><a href="#">Henry IV, Part 1</a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    </div>
+</div>
+<!--footer end -->
+
+</body>
+</html>
